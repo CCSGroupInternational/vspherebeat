2f242f615097c9a30d1959696f67b1eb2b8df953
“Fabio <fsantos@ccsint.eu>
2019-01-18 15:29:46 +0000

    Add VirtualMachines

--- module/performancemanager/virtualmachines/virtualmachines.go
+++ module/performancemanager/virtualmachines/virtualmachines.go
@@ -1,9 +1,13 @@
 package virtualmachines
 
 import (
-	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/libbeat/common/cfgwarn"
 	"github.com/elastic/beats/metricbeat/mb"
+	"github.com/CCSGroupInternational/vsphere-perfmanager/config"
+	 pm "github.com/CCSGroupInternational/vsphere-perfmanager/vspherePerfManager"
+	"time"
+	"github.com/elastic/beats/libbeat/common"
+	"github.com/vmware/govmomi/vim25/types"
 )
 
 // init registers the MetricSet with the central registry as soon as the program
@@ -20,7 +24,11 @@ func init() {
 // interface methods except for Fetch.
 type MetricSet struct {
 	mb.BaseMetricSet
-	counter int
+	Period   time.Duration
+	Hosts    []string
+	Username string
+	Password string
+	Insecure bool
 }
 
 // New creates a new instance of the MetricSet. New is responsible for unpacking
@@ -28,14 +36,24 @@ type MetricSet struct {
 func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 	cfgwarn.Experimental("The performancemanager virtualmachines metricset is experimental.")
 
-	config := struct{}{}
+	config := struct{
+		Period   time.Duration `config:"period"`
+		Hosts    []string      `config:"hosts"`
+		Username string        `config:"username"`
+		Password string        `config:"password"`
+		Insecure bool          `config:"insecure"`
+	}{}
 	if err := base.Module().UnpackConfig(&config); err != nil {
 		return nil, err
 	}
 
 	return &MetricSet{
 		BaseMetricSet: base,
-		counter:       1,
+		Period:   config.Period,
+		Hosts:    config.Hosts,
+		Username: config.Username,
+		Password: config.Password,
+		Insecure: config.Insecure,
 	}, nil
 }
 
@@ -43,10 +61,61 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 // format. It publishes the event which is then forwarded to the output. In case
 // of an error set the Error field of mb.Event or simply call report.Error().
 func (m *MetricSet) Fetch(report mb.ReporterV2) {
-	report.Event(mb.Event{
-		MetricSetFields: common.MapStr{
-			"counter": m.counter,
+	vspherePmConfig := config.VspherePerfManagerConfig{
+		Vcenter: config.Vcenter {
+			Username : m.Username,
+			Password : m.Password,
+			Host     : m.Hosts[0],
+			Insecure : m.Insecure,
 		},
-	})
-	m.counter++
+		QueryInterval: m.Period,
+		Metrics: map[config.EntitiesType][]config.MetricDef{
+			config.VMs: {
+				config.MetricDef{
+					Metric:   "net.packets*",
+					Entities: []string{"dropbox"},
+					Instance: []string{"vmnic\\d"},
+				},
+			},
+		},
+		Properties: []types.PropertySpec{{
+			Type: string(config.VMs),
+			PathSet: []string{"runtime.host"},
+		}},
+	}
+
+	vspherePerfManager, err := pm.Init(&vspherePmConfig)
+
+	if err == nil {
+
+	}
+
+	vms, err := vspherePerfManager.Vms()
+
+	if err == nil {
+
+	}
+
+	for _, vm := range vms {
+		for _, metric := range vm.Metrics {
+			report.Event(mb.Event{
+				MetricSetFields: common.MapStr{
+					"name"   : vm.GetProperty("name").(string),
+					"hostId" : vm.GetProperty("runtime.host").(types.ManagedObjectReference).Value,
+					"metric" : common.MapStr{
+						"info" : common.MapStr{
+							"metric"    : metric.Info.Metric,
+							"statsType" : metric.Info.StatsType,
+							"unitInfo"  : metric.Info.UnitInfo,
+						},
+						"result": common.MapStr{
+							"value"    : metric.Value.Value,
+							"instance" : metric.Value.Instance,
+						},
+					},
+				},
+			})
+		}
+
+	}
 }
