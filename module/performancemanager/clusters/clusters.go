338ec8853189ae97b56bf849b4f1aed647190235
“Fabio <fsantos@ccsint.eu>
2019-01-30 13:18:49 +0000

    Adda datacenter metadata to clusters metricset

--- module/performancemanager/clusters/clusters.go
+++ module/performancemanager/clusters/clusters.go
@@ -69,7 +69,9 @@ func (m *MetricSet) Fetch(report mb.ReporterV2) {
 			},
 			Samples: 6,
 			Data: map[string][]string{
-				string(pm.Clusters): {},
+				string(pm.Clusters): {"parent"},
+				"Folder": {"parent"},
+				string(pm.Datacenter): {},
 			},
 		},
 	}
@@ -84,10 +86,26 @@ func (m *MetricSet) Fetch(report mb.ReporterV2) {
 
 	for _, cluster := range clusters {
 		for _, metric := range cluster.Metrics {
+
+			var datacenter pm.ManagedObject
+			switch parentType := vspherePm.GetProperty(cluster, "parent").(pm.ManagedObject).Entity.Type; parentType {
+			case "Folder":
+				for {
+					parent := vspherePm.GetProperty(vspherePm.GetProperty(cluster, "parent").(pm.ManagedObject), "parent").(pm.ManagedObject)
+					if parent.Entity.Type == string(pm.Datacenter) {
+						datacenter = parent
+						break
+					}
+				}
+			case string(pm.Datacenter):
+				datacenter = vspherePm.GetProperty(cluster, "parent").(pm.ManagedObject)
+			}
+
 			report.Event(mb.Event{
 				MetricSetFields: common.MapStr{
 					"metaData": common.MapStr{
 						"name" : vspherePm.GetProperty(cluster, "name").(string),
+						"datacenter" : vspherePm.GetProperty(datacenter, "name").(string),
 					},
 					"metric" : common.MapStr{
 						"info" : common.MapStr{
